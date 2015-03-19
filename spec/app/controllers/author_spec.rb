e05bc2c5f81bbd3ae79d38c9848b9a3c754c1a76
hrl7 <hrlclb@gmail.com>
2015-03-19 20:37:34 +0900

    Fix test

---
+++ spec/app/controllers/author_spec.rb
@@ -0,0 +1,20 @@
+include Fabnavi
+require 'spec_helper'
+
+describe "AuthorController" do
+
+  describe "post '/'" do
+
+    context 'author controller' do
+      before do
+        
+      end
+
+
+      it 'first test' do
+        expect(author.name).to eq("")
+      end
+
+    end
+  end
+end
